95559a19ea18b7bbf869fac6925edbacaeeabb9c
luisvillanuevaf <lvillanuevaf@unsa.edu.pe>
2021-12-17 19:39:42 -0500

    delete files

--- a.py
+++ a.py
@@ -1,10 +1,1 @@
 print("Hola amigo como estas")
-def saludar (text):
-    return "Hola "+ text
-
-def multiple_saludo(text,anotherText):
-    message = saludar(text)
-    final_message = message + " " + anotherText
-    return final_message
-
-a = multiple_saludo("Luis ", "cuidate")
