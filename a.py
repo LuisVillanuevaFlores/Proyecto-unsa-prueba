95fc973c4b998ec72e74ddede2bf35a65174c0de
luisvillanuevaf <lvillanuevaf@unsa.edu.pe>
2021-12-20 06:57:34 -0500

    add poten

--- a.py
+++ a.py
@@ -1,2 +1,5 @@
 def sum (a,b):
     return a+b
+
+def multi (a,b):
+    return a*b
--- b.py
+++ b.py
@@ -1,7 +1,16 @@
 from a import sum
+from a import multi
 
 def sum_arr(arr):
     res = 0
     for i in range (len(arr)-1):
         res = res + sum (i,i+1)
     return res
+
+def potencia (num,exp):
+    if exp == 0:
+        return 1
+    return multi(num,potencia(num,exp-1))
+
+a = potencia(3,7)
+print(a)
